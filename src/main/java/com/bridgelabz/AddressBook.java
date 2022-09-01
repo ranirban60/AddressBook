940ca43d332a0fba1b4592330b319abfb61ce009
ranirban60 <ranirban60@gmail.com>
2022-09-02 00:09:40 +0530

    Added:Sort contact by name in AddressBook

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -1,43 +1,30 @@
 package com.bridgelabz;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AddressBook {
     static String name;
-    int xz;
     static boolean is_Running = false;
-    public HashMap<String, ContactInfo> addressBook;
+    public HashMap<String, ContactInfo> addressBook = new HashMap<>();
     public ArrayList<ContactInfo> listOfContacts = new ArrayList<>();
-
-
-
-    public AddressBook() {
-        addressBook = new HashMap<>();
-    }
 
     //Driver code
     public static void main(String[] args) {
-
-
-        HashMap<String, AddressBook> multiAdressBook = new HashMap<>();
+        HashMap<String, AddressBook> multiAddressBook = new HashMap<>();
         System.out.println("Welcome to the ADDRESS BOOK");
-
-        AddressBook obj=new AddressBook();
+        AddressBook obj = new AddressBook();
 
         AddressBook addressBookObj1 = new AddressBook();
         AddressBook addressBookObj2 = new AddressBook();
         AddressBook addressBookObj3 = new AddressBook();
-        multiAdressBook.put("AB1", addressBookObj1);
-        multiAdressBook.put("AB2", addressBookObj2);
-        multiAdressBook.put("AB3", addressBookObj3);
-
-        for (Map.Entry<String,AddressBook> addressbookEntry : multiAdressBook.entrySet()) {
-            addressbookEntry.getKey();
-        }
+        multiAddressBook.put("AB1", addressBookObj1);
+        multiAddressBook.put("AB2", addressBookObj2);
+        multiAddressBook.put("AB3", addressBookObj3);
 
         while (!is_Running) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter 1 ,2 ,3 for diff addressBook and 4 to exit");
+            System.out.println("Enter 1 for addressBook1, 2 for addressBook2, 3 for addressBook3 and 4 to exit");
             int option = scanner.nextInt();
             String key = switch (option) {
                 case 1 -> "AB1";
@@ -52,21 +39,21 @@ public class AddressBook {
                 ContactInfo contact = new ContactInfo();
                 contact.setContactInfo();
                 name = contact.firstName.toUpperCase(Locale.ROOT) + " " + contact.lastName.toUpperCase(Locale.ROOT);
-                if (multiAdressBook.get(key).addressBook.keySet().stream().noneMatch(k -> k.equals(name))){                 //JAVA STREAMS is used to check if any duplicate
-                    multiAdressBook.get(key).addressBook.put(name, contact);
-                    multiAdressBook.get(key).listOfContacts.add(contact);                                                                                                     //contact already exist in the addressBook
-                    multiAdressBook.get(key).addressBook.get(name).displayContactInfo();
-                }
-                else System.out.println("Contact already exist duplicate not allowed");
+                if (multiAddressBook.get(key).addressBook.keySet().stream().noneMatch(k -> k.equals(name))) {                 //JAVA STREAMS is used to check if any duplicate
+                    multiAddressBook.get(key).addressBook.put(name, contact);
+                    multiAddressBook.get(key).listOfContacts.add(contact);                                                                                                     //contact already exist in the addressBook
+                    multiAddressBook.get(key).addressBook.get(name).displayContactInfo();
+                } else System.out.println("Contact already exist duplicate not allowed");
             } else if (choice == 2) {
                 is_Running = true;
             } else if (choice == 3) {
-                multiAdressBook.get(key).editContact();
+                multiAddressBook.get(key).editContact();
             } else if (choice == 4) {
-                multiAdressBook.get(key).deleteContact();
+                multiAddressBook.get(key).deleteContact();
             }
         }
-        obj.searchContactBasedOnCity(multiAdressBook);
+        obj.searchContactBasedOnCity(multiAddressBook);
+        obj.sortContactsByPersonName(multiAddressBook);
     }
 
     /**
@@ -127,109 +114,56 @@ public class AddressBook {
     }
 
     /**
-     * UC8 Method
+     * UC8 Method for searching contacts based on city
      */
-    public void searchContactBasedOnCity(HashMap<String,AddressBook> multiAddressBook){
+    public void searchContactBasedOnCity(HashMap<String, AddressBook> multiAddressBook) {
 
-        HashMap<String,String> personCityDictionary = new HashMap<>();
+        HashMap<String, String> personCityDictionary = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the city to search the contacts based on city");
         String searchCity = scanner.nextLine();
-        int counter=0;
+        int counter = 0;
 
-        for (Map.Entry<String,AddressBook> addressBookEntry: multiAddressBook.entrySet()) {
+        for (Map.Entry<String, AddressBook> addressBookEntry : multiAddressBook.entrySet()) {
             addressBookEntry.getKey();
             AddressBook currentAddressBook = addressBookEntry.getValue();
-            for (ContactInfo item: currentAddressBook.listOfContacts) {
-                System.out.println();
+            for (ContactInfo item : currentAddressBook.listOfContacts) {
+                System.out.println("Contact details: ");
                 System.out.println(item.showContact());
             }
 
         }
 
-        for (Map.Entry<String,AddressBook> addressBookEntry: multiAddressBook.entrySet()) {
+        for (Map.Entry<String, AddressBook> addressBookEntry : multiAddressBook.entrySet()) {
             System.out.println(addressBookEntry.getKey());
-            for (Map.Entry<String,ContactInfo> contactEntry: addressBookEntry.getValue().addressBook.entrySet()) {
+            for (Map.Entry<String, ContactInfo> contactEntry : addressBookEntry.getValue().addressBook.entrySet()) {
                 String result = addressBookEntry.getValue().addressBook.get(contactEntry.getKey()).showContact();
                 System.out.println(contactEntry.getKey());
-                if (result.contains(searchCity)){
-                    personCityDictionary.put(contactEntry.getKey(),searchCity);
+                if (result.contains(searchCity)) {
+                    personCityDictionary.put(contactEntry.getKey(), searchCity);
                     counter++;
                 }
             }
         }
-        for (Map.Entry<String,String> pEntry : personCityDictionary.entrySet() ) {
-            System.out.println("Key= "+pEntry.getKey()+", Value= "+ pEntry.getValue());
+        for (Map.Entry<String, String> pEntry : personCityDictionary.entrySet()) {
+            System.out.println("Key= " + pEntry.getKey() + ", Value= " + pEntry.getValue());
         }
-        System.out.println("No of contacts from the searched city were: "+counter);
-    }
-}
-
-/**
- * Template class for creating a contact
- */
-class ContactInfo {
-    String firstName, lastName, address, city, state, zipcode, phoneNo, email;
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public void setPhoneNo(String phoneNo) {
-        this.phoneNo = phoneNo;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+        System.out.println("No of contacts from the searched city were: " + counter);
     }
 
     /**
-     * Method to save contact details
+     * Method for sorting contacts based on Person Name
+     *
+     * @param multiAddressBook HashMap containing all addressBooks is passed as a parameter
      */
-    public void setContactInfo() {
+    public void sortContactsByPersonName(HashMap<String, AddressBook> multiAddressBook) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter First Name: \n Last Name: \n Address: \n City: \n State: \n Zipcode: \n PhoneNO: \n Email: \n");
-        setFirstName(sc.nextLine());
-        setLastName(sc.nextLine());
-        setAddress(sc.nextLine());
-        setCity(sc.nextLine());
-        setState(sc.nextLine());
-        setZipcode(sc.nextLine());
-        setPhoneNo(sc.nextLine());
-        setEmail(sc.nextLine());
-    }
-
-    /**
-     * Method to display the contact details
-     */
-    public void displayContactInfo() {
-        System.out.print(" First Name: " + firstName + "\n Last Name: " + lastName + "\n Address: " + address +
-                "\n City: " + city + "\n State: " + state + "\n Zipcode: " + zipcode + "\n PhoneNO: " + phoneNo + "\n Email: " + email + "\n");
-    }
-
-    public String showContact(){
-        return  " First Name: " + firstName + "\n Last Name: " + lastName + "\n Address: " + address +
-                "\n City: " + city + "\n State: " + state + "\n Zipcode: " + zipcode + "\n PhoneNO: " + phoneNo + "\n Email: " + email + "\n";
+        for (Map.Entry<String, AddressBook> addressBookMapEntry : multiAddressBook.entrySet()) {
+            List<ContactInfo> sortedContacts = addressBookMapEntry.getValue().addressBook.values().stream().sorted(Comparator.comparing(contactInfo -> contactInfo.firstName + contactInfo.lastName)).collect(Collectors.toList());
+            System.out.println("Sorted Contacts By name : ");
+            for (ContactInfo item : sortedContacts) {
+                System.out.println(item.getFirstName() + " " + item.getLastName());
+            }
+        }
     }
 }
---
+++ src/main/java/com/bridgelabz/ContactInfo.java
@@ -0,0 +1,106 @@
+package com.bridgelabz;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Template class for creating a contact
+ */
+class ContactInfo {
+    String firstName, lastName, address, city, state, zipcode, phoneNo, email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Method to save contact details
+     */
+    public void setContactInfo() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Name: \n Last Name: \n Address: \n City: \n State: \n Zipcode: \n PhoneNO: \n Email: \n");
+        setFirstName(sc.nextLine());
+        setLastName(sc.nextLine());
+        setAddress(sc.nextLine());
+        setCity(sc.nextLine());
+        setState(sc.nextLine());
+        setZipcode(sc.nextLine());
+        setPhoneNo(sc.nextLine());
+        setEmail(sc.nextLine());
+    }
+
+    /**
+     * Method to display the contact details
+     */
+    public void displayContactInfo() {
+        System.out.print(" First Name: " + firstName + "\n Last Name: " + lastName + "\n Address: " + address +
+                "\n City: " + city + "\n State: " + state + "\n Zipcode: " + zipcode + "\n PhoneNO: " + phoneNo + "\n Email: " + email + "\n");
+    }
+
+    public String showContact() {
+        return " First Name: " + firstName + "\n Last Name: " + lastName + "\n Address: " + address +
+                "\n City: " + city + "\n State: " + state + "\n Zipcode: " + zipcode + "\n PhoneNO: " + phoneNo + "\n Email: " + email + "\n";
+    }
+}
