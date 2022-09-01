830fa23f3325af101506831ce930c90cfe22f0ee
ranirban60 <ranirban60@gmail.com>
2022-09-02 00:02:06 +0530

    Added:Count contact based on city in AddressBook

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 public class AddressBook {
     static String name;
+    int xz;
     static boolean is_Running = false;
     public HashMap<String, ContactInfo> addressBook;
-    public HashMap<String, AddressBook> multiAdressBook = new HashMap<>();
+    public ArrayList<ContactInfo> listOfContacts = new ArrayList<>();
+
 
 
     public AddressBook() {
@@ -15,6 +17,9 @@ public class AddressBook {
 
     //Driver code
     public static void main(String[] args) {
+
+
+        HashMap<String, AddressBook> multiAdressBook = new HashMap<>();
         System.out.println("Welcome to the ADDRESS BOOK");
 
         AddressBook obj=new AddressBook();
@@ -22,13 +27,17 @@ public class AddressBook {
         AddressBook addressBookObj1 = new AddressBook();
         AddressBook addressBookObj2 = new AddressBook();
         AddressBook addressBookObj3 = new AddressBook();
-        obj.multiAdressBook.put("AB1", addressBookObj1);
-        obj.multiAdressBook.put("AB2", addressBookObj2);
-        obj.multiAdressBook.put("AB3", addressBookObj3);
+        multiAdressBook.put("AB1", addressBookObj1);
+        multiAdressBook.put("AB2", addressBookObj2);
+        multiAdressBook.put("AB3", addressBookObj3);
+
+        for (Map.Entry<String,AddressBook> addressbookEntry : multiAdressBook.entrySet()) {
+            addressbookEntry.getKey();
+        }
 
         while (!is_Running) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter 1 for addressBook1, 2 for addressBook2, 3 for addressBook3 and 4 to exit");
+            System.out.println("Enter 1 ,2 ,3 for diff addressBook and 4 to exit");
             int option = scanner.nextInt();
             String key = switch (option) {
                 case 1 -> "AB1";
@@ -37,32 +46,34 @@ public class AddressBook {
                 default -> null;
             };
             if (option == 4) break;
-            System.out.println(" Enter\n 1 to create a new contact \n 2 to exit \n 3 to edit existing contact \n 4 to delete an existing contact");
+            System.out.println(" Enter \n 1 to create a new contact \n 2 to exit \n 3 to edit existing contact \n 4 to delete an existing contact");
             int choice = scanner.nextInt();
             if (choice == 1) {
                 ContactInfo contact = new ContactInfo();
                 contact.setContactInfo();
                 name = contact.firstName.toUpperCase(Locale.ROOT) + " " + contact.lastName.toUpperCase(Locale.ROOT);
-                if (obj.multiAdressBook.get(key).addressBook.keySet().stream().noneMatch(k -> k.equals(name))){                 //JAVA STREAMS is used to check if any duplicate
-                    obj.multiAdressBook.get(key).addressBook.put(name, contact);                                                //contact already exist in the addressBook
-                    obj.multiAdressBook.get(key).addressBook.get(name).displayContactInfo();
+                if (multiAdressBook.get(key).addressBook.keySet().stream().noneMatch(k -> k.equals(name))){                 //JAVA STREAMS is used to check if any duplicate
+                    multiAdressBook.get(key).addressBook.put(name, contact);
+                    multiAdressBook.get(key).listOfContacts.add(contact);                                                                                                     //contact already exist in the addressBook
+                    multiAdressBook.get(key).addressBook.get(name).displayContactInfo();
                 }
                 else System.out.println("Contact already exist duplicate not allowed");
             } else if (choice == 2) {
                 is_Running = true;
             } else if (choice == 3) {
-                obj.multiAdressBook.get(key).editContact();
+                multiAdressBook.get(key).editContact();
             } else if (choice == 4) {
-                obj.multiAdressBook.get(key).deleteContact();
+                multiAdressBook.get(key).deleteContact();
             }
         }
-        obj.searchContactBasedOnCity();
+        obj.searchContactBasedOnCity(multiAdressBook);
     }
 
     /**
      * Method to delete an existing contact
      */
     public void deleteContact() {
+        int xc;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the first and last name of the contact you want to delete from AddressBook: ");
         String name = scanner.nextLine().toUpperCase(Locale.ROOT);
@@ -116,28 +127,41 @@ public class AddressBook {
     }
 
     /**
-     * UC8,9,10 Methods for searching contacts based on city or state
+     * UC8 Method
      */
-    public void searchContactBasedOnCity(){
+    public void searchContactBasedOnCity(HashMap<String,AddressBook> multiAddressBook){
 
         HashMap<String,String> personCityDictionary = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the city to search the contacts based on city");
         String searchCity = scanner.nextLine();
+        int counter=0;
 
-        for (Map.Entry<String,AddressBook> addressBookEntry: multiAdressBook.entrySet()) {
+        for (Map.Entry<String,AddressBook> addressBookEntry: multiAddressBook.entrySet()) {
+            addressBookEntry.getKey();
+            AddressBook currentAddressBook = addressBookEntry.getValue();
+            for (ContactInfo item: currentAddressBook.listOfContacts) {
+                System.out.println();
+                System.out.println(item.showContact());
+            }
+
+        }
+
+        for (Map.Entry<String,AddressBook> addressBookEntry: multiAddressBook.entrySet()) {
             System.out.println(addressBookEntry.getKey());
             for (Map.Entry<String,ContactInfo> contactEntry: addressBookEntry.getValue().addressBook.entrySet()) {
                 String result = addressBookEntry.getValue().addressBook.get(contactEntry.getKey()).showContact();
                 System.out.println(contactEntry.getKey());
                 if (result.contains(searchCity)){
                     personCityDictionary.put(contactEntry.getKey(),searchCity);
+                    counter++;
                 }
             }
         }
         for (Map.Entry<String,String> pEntry : personCityDictionary.entrySet() ) {
-            System.out.println("Name= "+pEntry.getKey()+", City= "+ pEntry.getValue());
+            System.out.println("Key= "+pEntry.getKey()+", Value= "+ pEntry.getValue());
         }
+        System.out.println("No of contacts from the searched city were: "+counter);
     }
 }
 
