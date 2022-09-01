a32a94151469d074d834a7d430812ac8dcc5e453
ranirban60 <ranirban60@gmail.com>
2022-09-01 22:20:45 +0530

    Added: Edit Contact

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -1,31 +1,78 @@
 package com.bridgelabz;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class AddressBook{
     static String name;
     static boolean is_Running=false;
+    static  HashMap<String,ContactInfo> addressBook = new HashMap<>();
 
     public static void main(String[] args){
 
+        AddressBook addressBookObj = new AddressBook();
         System.out.println("Welcome to the ADDRESS BOOK");
-        HashMap<String,ContactInfo> addressBook = new HashMap<>();
+
 
         while (!is_Running) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter 1 to create a new contact and 2 to exit");
+            System.out.println("Enter 1 to create a new contact , 2 to exit , 3 to edit existing contact");
             int choice = scanner.nextInt();
             if (choice == 1) {
                 ContactInfo contact = new ContactInfo();
                 contact.setContactInfo();
-                name = contact.firstName + " " + contact.lastName;
+                name = contact.firstName.toUpperCase(Locale.ROOT) + " " + contact.lastName.toUpperCase(Locale.ROOT);
                 addressBook.put(name, contact);
                 addressBook.get(name).displayContactInfo();
             }else if (choice==2){
                 is_Running = true;
+            }else if (choice==3){
+                addressBookObj.editContact();
+                addressBook.get(name).displayContactInfo();
             }
         }
+    }
+
+
+    public void editContact(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your first name and Last name  : ");
+        String name = sc.nextLine().toUpperCase(Locale.ROOT);
+        if(addressBook.containsKey(name)) {
+            System.out.println("Enter the number you want to edit\n1.Address\n2.City\n3.State\n4.Zipcode\n5.Phone Number\n6.Email");
+            int number = sc.nextInt();
+            sc.nextLine();
+            switch (number){
+                case 1 -> {
+                    System.out.println("Enter new Address");
+                    addressBook.get(name).setAddress(sc.nextLine());
+                }
+                case 2 -> {
+                    System.out.println("Enter new City");
+                    addressBook.get(name).setCity(sc.nextLine());
+                }
+                case 3 -> {
+                    System.out.println("Enter new State");
+                    addressBook.get(name).setState(sc.nextLine());
+                }
+                case 4 -> {
+                    System.out.println("Enter new ZipCode");
+                    addressBook.get(name).setZipcode(sc.nextLine());
+                }
+                case 5 -> {
+                    System.out.println("Enter new Phone number");
+                    addressBook.get(name).setPhoneNo(sc.nextLine());
+                }
+                case 6 -> {
+                    System.out.println("Enter new Email");
+                    addressBook.get(name).setEmail(sc.nextLine());
+                }
+                default -> System.out.println("Please input a valid number (1-6)");
+            }
+        }else System.out.println("Contact not found");
+
     }
 }
 
@@ -59,8 +106,8 @@ class ContactInfo{
 
     public void setContactInfo() {
 
-        Scanner sc = new Scanner(System.in);//Scanner is an input stream to be scanned
-        System.out.print("Enter\n First Name: \n Last Name: \n Address: \n City: \n State: \n Zipcode: \n PhoneNO: \n Email: \n");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Name: \n Last Name: \n Address: \n City: \n State: \n Zipcode: \n PhoneNO: \n Email: \n");
         setFirstName(sc.nextLine());
         setLastName(sc.nextLine());
         setAddress(sc.nextLine());
